9354b4cb3099ded16a454b2830ff85418cc22baf
Nehemiah I. Dacres <vivacarlie@gmail.com>
2020-12-27 16:40:25 -0600

    beginning of turn based strategy game

---
+++ Battle.gd
@@ -0,0 +1,17 @@
+extends CSGCombiner
+
+
+enum  Phases { dispatch, execute, cleanup }
+
+
+# Called when the node enters the scene tree for the first time.
+func _init():
+	pass
+
+
+func _ready():
+	pass 
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	passs 
